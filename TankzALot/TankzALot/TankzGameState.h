ab0a18fb5c2566c4ec1098bebcd9e6aeae5d2486
Novacoast User <novacoastuser@Novacoasts-MacBook-Pro.local>
2014-04-12 02:18:12 -0700

    added some state files

---
+++ TankzALot/TankzALot/TankzGameState.h
@@ -0,0 +1,24 @@
+//
+//  TankzGameState.h
+//  TankzALot
+//
+//  Created by Novacoast User on 4/12/14.
+//  Copyright (c) 2014 LAHacks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    TankzPlayingStateReady = 0,
+    TankzPlayingStateRunning = 1,
+    TankzPlayingStateDone = 2
+}TankzPlayingState;
+
+@interface TankzGameState : NSObject
+
+
+@property (strong,nonatomic) NSMutableArray *playerList;
+@property (nonatomic) int turn;
+@property (nonatomic) TankzPlayingState playingState;
+
+@end
