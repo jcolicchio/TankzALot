4b17154ea8a352a2719c4e1f15951f3e778cb08d
Novacoast User <novacoastuser@Novacoasts-MacBook-Pro.local>
2014-04-12 02:18:12 -0700

    added some state files

---
+++ TankzALot/TankzALot/TankzGameState.m
@@ -0,0 +1,16 @@
+//
+//  TankzGameState.m
+//  TankzALot
+//
+//  Created by Novacoast User on 4/12/14.
+//  Copyright (c) 2014 LAHacks. All rights reserved.
+//
+
+#import "TankzGameState.h"
+
+@implementation TankzGameState
+
+
+@end
+
+
