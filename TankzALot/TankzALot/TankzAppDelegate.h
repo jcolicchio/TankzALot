24eff3722307de26476c03730e70e49d68c173a7
Joseph Colicchio <jcolicchio@novacoast.com>
2014-04-11 23:50:14 -0700

    Initial setup of Xcode

---
+++ TankzALot/TankzALot/TankzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TankzAppDelegate.h
+//  TankzALot
+//
+//  Created by Joseph Colicchio on 4/11/14.
+//  Copyright (c) 2014 LAHacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TankzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TankzALot/TankzALot/TankzViewController.m
@@ -0,0 +1,29 @@
+//
+//  TankzViewController.m
+//  TankzALot
+//
+//  Created by Joseph Colicchio on 4/11/14.
+//  Copyright (c) 2014 LAHacks. All rights reserved.
+//
+
+#import "TankzViewController.h"
+
+@interface TankzViewController ()
+
+@end
+
+@implementation TankzViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
